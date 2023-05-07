6587ff36ce28e5d7c43e1b8d07b5b685d123e180
Chin-Yun Yu <lolimaster.cs03@nctu.edu.tw>
2023-05-07 13:44:15 +0100

    update comments

--- render.cpp
+++ render.cpp
@@ -1,17 +1,3 @@
-/*
- ____  _____ _        _
-| __ )| ____| |      / \
-|  _ \|  _| | |     / _ \
-| |_) | |___| |___ / ___ \
-|____/|_____|_____/_/   \_\
-
-http://bela.io
-
-C++ Real-Time Audio Programming with Bela - Lecture 5: Classes and Objects
-additive-synth: an example implementing an additive synthesiser based on an
-				array of Wavetable oscillator objects
-*/
-
 #include <Bela.h>
 #include <libraries/Gui/Gui.h>
 #include <libraries/GuiController/GuiController.h>
@@ -32,7 +18,7 @@ const unsigned int kHopSize = 32;
 const unsigned int kBufferSize = 2048;
 const unsigned int kNeighbours = 24;
 
-// Circular buffer and pointer for assembling a window of samples
+// Circular buffer and pointer for the audio input
 std::vector<float> gOutputBuffer[2];
 int gOutputBufferPointer = 0;
 int gHopCounter = 0;
@@ -79,17 +65,20 @@ float gCachedX, gCachedY, gCachedZ;
 void weighted_hrir_background(void *)
 {
 	std::vector<float> point{gCachedX, gCachedY, gCachedZ};
+	// Find k simplices that could contain the point
 	auto simplices = gCoordTree.knn(point, kNeighbours);
 
 	std::vector<float> g(3);
 	int simplex = -1;
 	for (auto &s : simplices)
 	{
+		// Calculate the interpolation weights
 		std::fill(g.begin(), g.end(), 0.);
 		for (unsigned int i = 0; i < 9; i++)
 		{
 			g[i / 3] += gInverseMatrices[s][i] * point[i % 3];
 		}
+		// Check if the weights are positive
 		if (std::all_of(g.cbegin(), g.cend(), [](float x)
 						{ return x >= 0; }))
 		{
@@ -98,6 +87,7 @@ void weighted_hrir_background(void *)
 		}
 	}
 
+	// If no proper simplex is found, return
 	if (simplex < 0)
 	{
 		gErrorCounts++;
@@ -105,14 +95,17 @@ void weighted_hrir_background(void *)
 		return;
 	}
 
+	// get the HRIRs indexes
 	auto interpolationIndexes = gSimplices[simplex];
 
+	// normalize the weights
 	float sum = g[0] + g[1] + g[2];
 	std::for_each(g.begin(), g.end(), [sum](float &x)
 				  { x /= sum; });
 
 	std::vector<float> weightedIR(gHRIRTruncatedSamples);
 
+	// interpolate the HRIRs
 	for (unsigned int c = 0; c < 2; c++)
 	{
 		std::fill(weightedIR.begin(), weightedIR.end(), 0.0);
@@ -129,8 +122,10 @@ void weighted_hrir_background(void *)
 void nearest_hrir_background(void *)
 {
 	std::vector<float> point{gCachedX, gCachedY, gCachedZ};
+	// Find the nearest HRIR
 	auto index = gCoordTree.nearest_index(point);
 
+	// Copy the HRIRs
 	std::copy(gHRIRInterleaved[index * 2].begin(), gHRIRInterleaved[index * 2].end(), gSampledHRIR[0].begin());
 	std::copy(gHRIRInterleaved[index * 2 + 1].begin(), gHRIRInterleaved[index * 2 + 1].end(), gSampledHRIR[1].begin());
 }
@@ -160,6 +155,7 @@ bool setup(BelaContext *context, void *userData)
 		return false;
 	}
 
+	// Read the HRIR coordinates and the neighbour simplices
 	while (std::getline(fs, line))
 	{
 		parse << line;
@@ -188,6 +184,7 @@ bool setup(BelaContext *context, void *userData)
 
 	float w;
 	int s1, s2, s3;
+	// Read the inverse matrices and the simplices
 	while (std::getline(fs, line))
 	{
 		parse << line;
@@ -202,6 +199,7 @@ bool setup(BelaContext *context, void *userData)
 	}
 	fs.close();
 
+	// Calculate the centres of the simplices
 	for (auto &simplex : gSimplices)
 	{
 		std::vector<float> centre(3, 0.0);
@@ -222,6 +220,7 @@ bool setup(BelaContext *context, void *userData)
 	rt_printf("Total number of HRIRs: %d\n", gHRIRCoordinates.size());
 	rt_printf("Total number of simplices: %d\n", gSimplices.size());
 
+	// Load the HRIRs
 	for (int n = 0; n < gHRIRCoordinates.size(); n++)
 	{
 		auto hrirFile = gHRIRDir + "/" + std::to_string(n) + ".wav";
@@ -232,6 +231,7 @@ bool setup(BelaContext *context, void *userData)
 			gHRIRTruncatedSamples = hrir[0].size();
 	}
 
+	// Set up the buffers
 	for (unsigned int c = 0; c < 2; c++)
 	{
 		gSampledHRIR[c].resize(gHRIRTruncatedSamples);
@@ -241,7 +241,12 @@ bool setup(BelaContext *context, void *userData)
 
 	// Set up the KD tree for sample retrieval
 	gCoordTree = KDTree(gSimplicesCentres);
+	// Set up the thread for the HRIR interpolation
+	gSampleRetrieveTask = Bela_createAuxiliaryTask(weighted_hrir_background, 90, "bela-retrieve-hrir");
+
+	// uncomment the following lines and comment the previous two lines to use nearest neighbour instead of interpolation
 	// gCoordTree = KDTree(gHRIRCoordinates);
+	// gSampleRetrieveTask = Bela_createAuxiliaryTask(nearest_hrir_background, 90, "bela-retrieve-hrir");
 
 	// Set up the GUI
 	gGui.setup(context->projectName);
@@ -252,9 +257,6 @@ bool setup(BelaContext *context, void *userData)
 	gGuiController.addSlider("Azimuth", 0, -180, 180, 0);
 	gGuiController.addSlider("Radius", gRadius, 0.3, 2, 0);
 
-	// Set up the thread for the HRIR interpolation
-	gSampleRetrieveTask = Bela_createAuxiliaryTask(weighted_hrir_background, 90, "bela-retrieve-hrir");
-
 	return true;
 }
 
@@ -264,9 +266,11 @@ void render(BelaContext *context, void *userData)
 	float az = gGuiController.getSliderValue(1);
 	float radius = gGuiController.getSliderValue(2);
 
+	// convert degrees to radians
 	float col = map(90 - el, 0, 180, 0, M_PI);
 	az = -map(az, -180, 180, -M_PI, M_PI);
 
+	// calculate the cartesian coordinates
 	float col_sin = sin(col);
 	float col_cos = cos(col);
 	float az_sin = sin(az);
@@ -275,15 +279,18 @@ void render(BelaContext *context, void *userData)
 	gY = col_sin * az_sin;
 	gZ = col_cos;
 
+	// use distance attenuation
 	float scaler = gRadius / radius;
 
 	for (unsigned int n = 0; n < context->audioFrames; n++)
 	{
-		// float in = gSoundBuffer[gReadPointer] * scaler;
+		// Read the audio input and convert to mono
 		float in = (audioRead(context, n, 0) + audioRead(context, n, 1)) * scaler;
+		// store the input in the output buffer
 		gOutputBuffer[0][gOutputBufferPointer] = in;
 		gOutputBuffer[1][gOutputBufferPointer] = in;
 
+		// HRIR convolution
 		for (unsigned int c = 0; c < 2; c++)
 		{
 			float out = 0.0;
@@ -294,14 +301,17 @@ void render(BelaContext *context, void *userData)
 			audioWrite(context, n, c, out);
 		}
 
+		// get new HRIR samples
 		if (++gHopCounter >= kHopSize)
 		{
 			gHopCounter = 0;
+			// calculate the interpolation gradients for sample-level interpolation
 			for (unsigned int c = 0; c < 2; c++)
 				std::transform(gCurrentHRIR[c].cbegin(), gCurrentHRIR[c].cend(), gSampledHRIR[c].cbegin(),
 							   gHRIRInterpolationGrad[c].begin(), [](const float &cur, const float &next)
 							   { return (next - cur) / kHopSize; });
 
+			// fire the next sample retrieval task if the coordinates have changed
 			if ((gCachedX != gX) || (gCachedY != gCachedY) || (gCachedZ != gZ))
 			{
 				gCachedX = gX;
@@ -311,9 +321,11 @@ void render(BelaContext *context, void *userData)
 			}
 		}
 
+		// interpolate the HRIR samples through time
 		for (unsigned int c = 0; c < 2; c++)
 			std::transform(gCurrentHRIR[c].cbegin(), gCurrentHRIR[c].cend(), gHRIRInterpolationGrad[c].cbegin(), gCurrentHRIR[c].begin(), std::plus<float>());
 
+		// increment the output buffer pointer
 		gOutputBufferPointer++;
 		if (gOutputBufferPointer >= kBufferSize)
 			gOutputBufferPointer = 0;
